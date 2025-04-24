8e400c26a39ff4ab9a2125feca4fc8a3c8394c69
Ikami-Mercy <mercyikami95@gmail.com>
2025-04-24 23:22:11 +0300

    Fix R class import issue

--- android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/MethodCallHandlerImpl.kt
+++ android/src/main/kotlin/io/github/ponnamkarthik/toast/fluttertoast/MethodCallHandlerImpl.kt
@@ -14,6 +14,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import kotlin.Exception
+import io.github.ponnamkarthik.toast.fluttertoast.R
 
 internal class MethodCallHandlerImpl(private var context: Context) : MethodCallHandler {
 
